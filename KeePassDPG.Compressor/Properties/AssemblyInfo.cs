1e20e93cbb7571476f10b708cd116837cda5f331
Quintero, Jose <jose.quintero@nucor.com>
2022-01-11 10:53:37 -0500

    Reverted project to use self contained AssemblyInfo.cs files  and not use SharedAssemblyInfo.cs.

--- KeePassDPG.Compressor/Properties/AssemblyInfo.cs
+++ KeePassDPG.Compressor/Properties/AssemblyInfo.cs
@@ -4,8 +4,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KeePassDPG.Compressor")]
 [assembly: AssemblyDescription("Utility to compress word files to use with KeePass Dictionary Password Generator.")]
 [assembly: AssemblyConfiguration("")]
-
+[assembly: AssemblyCompany("Jose Quintero")]
 [assembly: AssemblyProduct("KeePassDPG")]
-
+[assembly: AssemblyCopyright("Copyright © Jose Quintero 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("582851e5-a6fe-46b5-95b8-f0b847411fd1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- KeePassDPG/Properties/AssemblyInfo.cs
+++ KeePassDPG/Properties/AssemblyInfo.cs
@@ -4,8 +4,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KeePassDPG")]
 [assembly: AssemblyDescription("A random dictionary password generator with the ability to substitute characters to obfuscate password plugin for KeePass.")]
 [assembly: AssemblyConfiguration("")]
-
+[assembly: AssemblyCompany("Jose Quintero")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-
+[assembly: AssemblyCopyright("Copyright © Jose Quintero 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4de394c2-7e1d-4286-b2c5-0c933d9769f8")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
